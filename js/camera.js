9abce3fb9a0ccbb6ec6fe863cd0d1eacb0204954
Joao Lino <linusmannuel@gmail.com>
2023-07-28 08:15:11 +0100

    captura de foto

---
+++ js/camera.js
@@ -0,0 +1,39 @@
+const botaoIniciarCamera = document.querySelector('[data-video-botao]');
+const campoCamera = document.querySelector('[data-camera]');
+const video = document.querySelector('[data-video]');
+const botaoTirarFoto = document.querySelector('[data-tirar-foto]');
+const canvas = document.querySelector('[data-video-canvas]');
+const mensagem = document.querySelector('[data-mensagem]');
+const botaoRepetirFoto = document.querySelector('[data-repetir-foto]');
+let imagemURL = '';
+
+botaoIniciarCamera.addEventListener('click', () => {
+	tirarFoto();
+	botaoIniciarCamera.style.display = 'none';
+	campoCamera.style.display = 'block';
+});
+
+botaoTirarFoto.addEventListener('click', (e) => {
+	tirarFoto();
+	campoCamera.style.display = 'none';
+	mensagem.style.display = 'block';
+	botaoRepetirFoto.style.display = 'block';
+});
+
+botaoRepetirFoto.addEventListener('click', (e) => {
+	tirarFoto();
+	campoCamera.style.display = 'block';
+	mensagem.style.display = 'none';
+	botaoRepetirFoto.style.display = 'none';
+});
+
+async function tirarFoto() {
+	const iniciarVideo = await navigator.mediaDevices.getUserMedia({
+		video: true,
+		audio: false,
+	});
+
+	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+	imagemURL = canvas.toDataURL('image/jpeg');
+	video.srcObject = iniciarVideo;
+}
